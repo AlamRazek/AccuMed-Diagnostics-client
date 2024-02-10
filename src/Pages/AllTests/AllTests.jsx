a049b46b56f64b699a562a44b02b8d0b9eeb3f13
AlamRazek <ohidul933gmail.com>
2024-02-10 05:17:51 +0000

    pagination

--- src/Pages/AllTests/AllTests.jsx
+++ src/Pages/AllTests/AllTests.jsx
@@ -52,6 +52,34 @@ const AllTests = () => {
           <TestCards key={data._id} data={data}></TestCards>
         ))}
       </div>
+      <div className="text-center my-2">
+        <div className="join ">
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="1"
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="2"
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="3"
+          />
+          <input
+            className="join-item btn btn-square"
+            type="radio"
+            name="options"
+            aria-label="4"
+          />
+        </div>
+      </div>
     </div>
   );
 };
